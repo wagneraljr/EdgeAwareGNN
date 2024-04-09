17b18c85ad48bb600d56f37ca501abe3862a6b9d
Wagner Almeida <69909128+wagneraljr@users.noreply.github.com>
2024-04-09 13:30:26 -0300

    Update calculations_util.py

--- src/utils/calculations_util.py
+++ src/utils/calculations_util.py
@@ -1,6 +1,5 @@
 import numpy as np
-from sklearn.metrics import mean_absolute_error, mean_squared_error
-
+from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_absolute_percentage_error
 
 class CalculationsUtil:
     @staticmethod
@@ -8,4 +7,5 @@ class CalculationsUtil:
     def compute_metrics(y_true, y_pred):
         mae = mean_absolute_error(y_true, y_pred)
         rmse = np.sqrt(mean_squared_error(y_true, y_pred))
+        mape = mean_absolute_percentage_error(y_true, y_pred)
         return {'MAE': mae, 'RMSE': rmse}
